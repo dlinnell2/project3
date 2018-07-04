892d7867cc74cbe6e260e53a4b96c3c1fddcfdd8
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-07-04 14:20:38 -0500

    File upload working

--- client/src/components/AddEmployee/AddEmployee.js
+++ client/src/components/AddEmployee/AddEmployee.js
@@ -2,9 +2,82 @@ import React from 'react';
 
 
 class AddEmployee extends React.Component {
-    render () {
+
+    state = {
+        images: 0,
+        firstName: '',
+        lastName: '',
+    };
+
+    handleInputChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleUploadImage = (e) => {
+        e.preventDefault();
+
+        const data = new FormData();
+        data.append('file', this.uploadInput.files[0]);
+
+        fetch('/api/admin/add/images', {
+            method: 'POST',
+            body: data,
+        }).then((res) => {
+            console.log(res);
+            this.setState({ images: this.state.images + 1 })
+        });
+    }
+
+    click = () => {
+        console.log('click');
+    }
+
+    render() {
         return (
-            <h1>Add Employee</h1>
+            <div>
+                <form onSubmit={this.handleUploadImage} className="center">
+
+                    <h2>Please enter the employee's name</h2>
+
+                    <input
+                        value={this.state.firstName}
+                        onChange={this.handleInputChange}
+                        name="firstName"
+                        placeholder="First Name"
+                    />
+
+                    <input
+                        value={this.state.lastName}
+                        onChange={this.handleInputChange}
+                        name="lastName"
+                        placeholder="Last Name"
+                    />
+
+                    <br /> <br />
+
+                    <h3>Please select images to upload</h3>
+                    <h4> A minimum of 5 is suggested </h4>
+
+                    <div className="btn btn-success">
+                        <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
+                    </div>
+
+                    <br /><br />
+
+                    <div>
+                        <button className="btn btn-primary">Upload</button>
+                    </div>
+
+                    <br />
+                    <h4>You have successfully uploaded {this.state.images} {this.state.images === 1 ? 'image' : 'images'}</h4>
+                    <br />
+
+                </form>
+                <button type='button' onClick={this.click}>Test</button>
+            </div>
         )
     }
 };
--- routes/api/admin.js
+++ routes/api/admin.js
@@ -5,29 +5,31 @@ const router = require("express").Router();
 /* // Matches with "/api/admin/all"
 router.route("/all")
   .get(employeeController.findAll);
-
-//Matches with "/api/admin/add/images"
-router.route("/add/images").post((req, res) =>{
-  req.files.file.mv(`${__dirname}/../../recognizer/addEmpImages/${req.files.file.name}`, function(err){
-      if (err) console.log(err);
-
-      console.log('success');
-
-      res.json({fileName:req.files.file.name});
-  });
-});
-
-// Matches with "/api/admin/add/recognize"
-router.route("/add/recognize")
-  .get(recognizer.addNew);
-
-// Matches with "/api/admin/add"
-router.route("/add")
-  .get(employeeController.create);
-
-router.route("/delete/:user")
-  .get(employeeController.remove);
-
   */
 
-module.exports = router;
+//Matches with "/api/admin/add/images"
+router.route("/add/images").post((req, res) => {
+  req.files.file.mv(`${__dirname}/../../recognizer/addEmpImages/${req.files.file.name}`, function (err) {
+    if (err) console.log(err);
+
+    console.log('success');
+
+    res.sendStatus(200);
+  })
+})
+
+  /*
+  // Matches with "/api/admin/add/recognize"
+  router.route("/add/recognize")
+    .get(recognizer.addNew);
+  
+  // Matches with "/api/admin/add"
+  router.route("/add")
+    .get(employeeController.create);
+  
+  router.route("/delete/:user")
+    .get(employeeController.remove);
+  
+    */
+
+  module.exports = router;
