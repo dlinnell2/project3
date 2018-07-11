c5f1ac6c39c19928151ebf1ed2ebccf88d712639
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-07-11 15:18:14 -0500

    Still trying

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,17 +3,14 @@ import axios from 'axios';
 export default {
 
     recognizeNewEmployee: (data) => {
-        console.log('api');
         return axios.post("/api/admin/add/recognize", data);
     },
 
     addEmployeeToDb: (data) => {
-        console.log('add');
         return axios.post('/api/admin/add', data);
     },
 
     saveState: () => {
-        console.log('save state')
         return axios.get('/api/admin/saveRecognizer');
     },
 
